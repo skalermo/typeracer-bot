1013ef82e01ffa0bb93fe5e3afe943079da33845
skalermo <skalermo@gmail.com>
2020-07-05 11:57:47 +0200

    Improve typing delay implementation Replace sleep with timer

--- bot.py
+++ bot.py
@@ -1,21 +1,23 @@
+from PySide2.QtCore import QTimer
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
-from time import sleep
 from pynput.keyboard import Controller
 import pyautogui
 
 
 class Bot:
     def __init__(self):
-        # self.timer = timer
-        # self.timer.timeout.connect()
+        self.current_timer = None
+        self._init_timer()
+
+        self._text_iter = None
         self.keyboard = Controller()
 
         # typing speed in wpm
         self.typing_speed = 100
         self.expected_time_s = 0
-        self.typing_delay = 1
+        self.typing_delay_s = 1
 
         self.text_to_type = ''
 
@@ -54,12 +56,14 @@ class Bot:
 
     def start_race(self):
         self._focus_on_input_box()
-        for char in self.text_to_type:
-            self._enter_character(char)
+        self._start_timer()
 
-    def _enter_character(self, char):
-        sleep(self.typing_delay)
-        self.keyboard.type(char)
+    def _enter_next_character(self):
+        try:
+            char = next(self._text_iter)
+            self.keyboard.type(char)
+        except StopIteration:
+            self._stop_timer()
 
     def update_speed(self, new_speed):
         self.typing_speed = new_speed
@@ -69,9 +73,25 @@ class Bot:
     def _calc_typing_delay(self):
         word_count = len(self.text_to_type.split())
         self.expected_time_s = word_count / self.typing_speed * 60
-        self.typing_delay = self.expected_time_s / len(self.text_to_type)
+        self.typing_delay_s = self.expected_time_s / len(self.text_to_type)
 
     def _focus_on_input_box(self):
+        last_pos = pyautogui.position()
         pyautogui.click(100, 100)
         element = self.driver.find_element_by_class_name('txtInput')
         element.click()
+        pyautogui.moveTo(*last_pos)
+
+    def _init_timer(self):
+        self.current_timer = QTimer()
+        self.current_timer.timeout.connect(self._enter_next_character)
+
+    def _start_timer(self):
+        self._text_iter = iter(self.text_to_type)
+        if self.current_timer.isActive():
+            self._stop_timer()
+        self.current_timer.start(self.typing_delay_s * 1000)
+
+    def _stop_timer(self):
+        self.current_timer.stop()
+
